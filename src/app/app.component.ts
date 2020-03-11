e43e9a8d6a966dabed75901190420a8f456b9fa6
Massimiliano Perelli <massimiliano.perelli.ext@siemens.com>
2020-03-11 16:20:18 +0100

    feat(app): push initial code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  title = 'mental-trainer';
+  isStarted = false;
+  currentClass = 'b-black';
+  private heartBeatTimer: any = undefined;
+
+  onStart(event: any): void {
+    if (this.heartBeatTimer == null) {
+      this.isStarted = true;
+      this.heartBeatTimer = setInterval(() => this.pickNextColor(), 3 * 1000);
+    }
+    event.stopPropagation();
+  }
+
+  onStop(): void {
+    if (this.heartBeatTimer != null) {
+      this.isStarted = false;
+      clearInterval(this.heartBeatTimer);
+      this.currentClass = 'b-black';
+      this.heartBeatTimer = null;
+    }
+  }
+
+  private pickNextColor() {
+    const nextIndex = Math.floor(Math.random() * (3 - 0) ) + 0;
+    switch (nextIndex) {
+      case 0: {
+        this.currentClass = 'b-red';
+        break;
+      }
+      case 1: {
+        this.currentClass = 'b-blue';
+        break;
+      }
+      case 2: {
+        this.currentClass = 'b-yellow';
+        break;
+      }
+      case 3: {
+        this.currentClass = 'b-green';
+        break;
+      }
+      default: {
+        this.currentClass = 'b-blue';
+        break;
+      }
+    }
+    setTimeout( () => { this.currentClass = 'b-black'; }, 1500 );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    HelloComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
